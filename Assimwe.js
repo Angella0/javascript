6f9401f274a90ec68eb73daa3a8941033147ae40
Angella0 <angellasimbwa@gmail.com>
2021-03-07 10:00:07 +0300

    first attempt

---
+++ Assimwe.js
@@ -0,0 +1,15 @@
+function list(){
+let fruit =['strawberry', 'tomato', 'kiwi', 'mango', 'guava'];
+fruit.forEach(element=>console.log(element));
+console.log(fruit[0]);
+
+
+
+}
+list();
+
+  
+
+
+
+
